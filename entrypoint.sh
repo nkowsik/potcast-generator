96d60377cc6a4c44c636c961d2be93fb397fed33
Kowsik Nalluri <7689172+nkowsik@users.noreply.github.com>
2025-03-06 10:00:01 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /usr/bin/rss.py
 git pull origin main
+python3 /usr/bin/rss.py
 git add -A && git commit -m "Update Feed"
 git push origin main
 
