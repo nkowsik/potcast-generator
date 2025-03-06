eeb86008006580e421e373908341fbd302d5e0a5
Kowsik Nalluri <7689172+nkowsik@users.noreply.github.com>
2025-03-06 09:44:13 -0500

    Create rss.py

---
+++ rss.py
@@ -0,0 +1,35 @@
+import feedparser 
+
+URLs = ["https://blog.bytebytego.com/feed"]
+
+items = []
+
+def generate_table(items):
+    table = PrettyTable()
+    table.field_names = ["Title", "Link", "Description"]
+    for item in items:
+        table.add_row([item['title'], item['link'], item['description']])
+    return table.get_string()
+
+def update_readme(readme_path, table_content):
+    with open(readme_path, 'r') as file:
+        content = file.readlines()
+
+
+
+    new_content = ['## Latest Articles\n', table_content, '\n']
+
+    with open(readme_path, 'w') as file:
+        file.writelines(new_content)
+
+for URL in URLs:
+    feed = feedparser.parse(URL)
+    for entry in feed.entries:
+        title = entry.title
+        link = entry.link
+        description = entry.description
+        pubDate = entry.published
+        items.append({'title': title, 'link': link, 'description': description, 'published': pubDate})
+
+table_content = generate_table(items)
+update_readme(readme_file, table_content)
